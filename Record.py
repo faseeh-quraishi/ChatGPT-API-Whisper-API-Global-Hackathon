4c2d44800444bb6dfb28303d73a886d9403233c8
faseeh-quraishi <faseeh.quraishi.fq@gmail.com>
2023-03-25 00:04:01 +0500

    First Commit

---
+++ Record.py
@@ -0,0 +1,38 @@
+# Audio Recording
+def AudioRecorder(secs):
+    import pyaudio
+    import wave
+    # Set the audio parameters
+    FORMAT = pyaudio.paInt16 
+    CHANNELS = 1
+    RATE = 16000
+    CHUNK = 1024
+    RECORD_SECONDS = secs
+
+    # Create a PyAudio object and open the microphone stream
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,
+                        frames_per_buffer=CHUNK)
+
+    # Create a list to store the recorded audio frames
+    frames = []
+
+    # Record audio from the microphone and store it in the frames list
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    # Stop the microphone stream and terminate the PyAudio object
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    # Create a new wave file from the recorded audio frames
+    wf = wave.open("recorded_audio.wav", "wb")
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(audio.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b"".join(frames))
+    wf.close()
+
---
+++ whisper.py
@@ -0,0 +1,18 @@
+from Record import AudioRecorder
+
+AudioRecorder(10)
+
+
+
+
+# Speech to Text
+
+# import openai
+# # fetching key
+# key = open('whisperAPI.txt','r').readline()
+# # print(key)
+# openai.api_key = key
+# with open("recorded_audio.wav", "rb") as f:
+#     transcript = openai.Audio().transcribe("whisper-1", f)
+# print(transcript['text'])
+
