de5146edc6e42ee24764f010e2cab9f3dfec8874
faseeh-quraishi <faseeh.quraishi.fq@gmail.com>
2023-03-25 18:42:32 +0500

    data file updated and whisper function added

--- chatGPT.py
+++ chatGPT.py
@@ -1,11 +1,13 @@
 import openai
 
 prompt = {}
-all_prompts = []
+messages = []
+# 
 f =open('GPT-3.5_data.txt','r')
 for line in f:
     line = line.strip('\n').split(':')
     prompt[line[0]] = line[1]
-    all_prompts.append(prompt)
+    messages.append(prompt)
     prompt = {}
-print('\n',all_prompts,'\n')
+for i in messages:
+    print('\n',i,'\n')
--- whisper.py
+++ whisper.py
@@ -4,15 +4,22 @@ AudioRecorder(10)
 
 
 
-
+def whisper():
 # Speech to Text
 
-# import openai
-# # fetching key
-# key = open('OpenAI-API.txt','r').readline()
-# # print(key)
-# openai.api_key = key
-# with open("recorded_audio.wav", "rb") as f:
-#     transcript = openai.Audio().transcribe("whisper-1", f)
-# whisper_resp = transcript['text']
+    import openai
+    # transcript = {'text':'I have frequent loose and watery stool, I feel abdominal pain and cramps.'}
+    
+    # fetching key
+    key = open('OpenAI-API.txt','r').readline()
+    openai.api_key = key
+    with open("recorded_audio.wav", "rb") as f:
+        transcript = openai.Audio().transcribe("whisper-1", f)
+    # return transcript['text']
 
+    print('Transcribed User Input: '+{transcript['text']})
+    prompt = open('GPT-3.5_data.txt','a+')
+    input = '\nuser: '+transcript['text']
+    prompt.write(input)
+
+whisper()
